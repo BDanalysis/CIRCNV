73596a9b7100fa7a042918d3e5eb07dc8341b4e4
BDanalysis <41245376+BDanalysis@users.noreply.github.com>
2021-04-24 18:27:01 +0800

    Delete segment.R

--- CIRCNV/segment.R
+++
@@ -1,7 +0,0 @@
-library(cghFLasso)
-"segment" <- function(data){
-   seg<-cghFLasso(data, FDR=0.01)
-   segdata<-seg$Esti.CopyN
-   out.file="seg.txt"
-   write.table(segdata, file=out.file, sep="\t")
-}
